27d5e28404719e98a28b55c3f1bf402ae041cdef
Ain <ain.salula@gmail.com>
2017-02-14 00:17:58 +0200

    parsing out a due date with regexp

--- ForgetTheMilk/src/ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/src/ForgetTheMilk/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,12 +14,21 @@ namespace ForgetTheMilk.Controllers
             return View(Tasks);
         }
 
-        public static readonly List<string> Tasks = new List<string>();
+        public static readonly List<Task> Tasks = new List<Task>();
 
         [HttpPost]
         public IActionResult Add(string task)
         {
-            Tasks.Add(task);
+            var taskItem = new Task {Description = task};
+            var dueDatePattern = new Regex(@"may\s(\d)");
+            var hasDueDate = dueDatePattern.IsMatch(task);
+            if (hasDueDate)
+            {
+                var dueDate = dueDatePattern.Match(task);
+                var day = Convert.ToInt32(dueDate.Groups[1].Value);
+                taskItem.DueDate = new DateTime(DateTime.Today.Year, 5, day);
+            }
+            Tasks.Add(taskItem);
             return RedirectToAction("Index");
         }
 
@@ -26,5 +36,11 @@ namespace ForgetTheMilk.Controllers
         {
             return View();
         }
+    }
+
+    public class Task
+    {
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
     }
 }
